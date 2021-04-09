a465578f4f8408daf54e4f499461e7f935d61caf
Jay Guo <jguo144@gmail.com>
2021-04-09 15:44:13 -0400

    Fix key error for resource_patch

--- ckanext/xloader/jobs.py
+++ ckanext/xloader/jobs.py
@@ -481,7 +481,8 @@ def update_resource(resource, patch_only=False):
     """
     action = 'resource_update' if not patch_only else 'resource_patch'
     from ckan import model
-    context = {'model': model, 'session': model.Session, 'ignore_auth': True}
+    site_user = get_action('get_site_user')({'ignore_auth': True}, {})
+    context = {'model': model, 'session': model.Session, 'ignore_auth': True, 'user': site_user.get('name')}
     get_action(action)(context, resource)
 
 
